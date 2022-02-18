02fa99ba4b5ec454ebadd4c32137ab0a0b4efd5d
ZSAndroid <1210762604@qq.com>
2022-02-18 13:21:15 +0800

    Initial commit

---
+++ mybatis-01/src/main/java/com/zsitking/dao/StudentDao.java
@@ -0,0 +1,13 @@
+package com.zsitking.dao;
+
+import com.zsitking.entity.Student;
+
+import java.util.List;
+
+/**
+ * 接口操作student表
+ */
+public interface StudentDao {
+    //查询student表全部数据
+    public List<Student> selectAllData();
+}
