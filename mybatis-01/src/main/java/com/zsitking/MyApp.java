2014faaccf0680315e74fa22d48befb34859278a
ZSAndroid <1210762604@qq.com>
2022-02-18 16:13:37 +0800

    增加Mybatis工具类的使用

--- mybatis-01/src/main/java/com/zsitking/MyApp.java
+++ mybatis-01/src/main/java/com/zsitking/MyApp.java
@@ -1,6 +1,7 @@
 package com.zsitking;
 
 import com.zsitking.entity.Student;
+import com.zsitking.utils.MybatisUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -15,11 +16,7 @@ import java.util.List;
  */
 public class MyApp {
     public static void main(String[] args) throws IOException {
-        String config = "mybatis-config.xml";//target/classes编译目录下的mybatis主配置文件
-        InputStream in = Resources.getResourceAsStream(config);//通过流方式读取数据库参数信息
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);//数据流传入SQL会话工程处理
-        SqlSession sqlSession = factory.openSession();//开启SQL会话
+        SqlSession sqlSession = MybatisUtil.getSqlSession();//开启SQL会话，非自动提交事务
         String sqlId = "com.zsitking.dao.StudentDao"+"."+"selectAllData";//全限定名称或mybatis命名空间(包名+类名+.+SQL唯一标识名)
         List<Student> students = sqlSession.selectList(sqlId);//查询全部集合数据
         for (Student stu: students) {//循环遍历
--- mybatis-01/src/test/java/com/zsitking/MybatisTest.java
+++ mybatis-01/src/test/java/com/zsitking/MybatisTest.java
@@ -18,10 +18,9 @@ public class MybatisTest {
         InputStream in = Resources.getResourceAsStream(config);//通过流方式读取数据库参数信息
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);//数据流传入SQL会话工程处理
-        SqlSession sqlSession = factory.openSession();//开启SQL会话
+        SqlSession sqlSession = factory.openSession(true);//开启SQL会话，自动提交事务
         String sqlId = "com.zsitking.dao.StudentDao"+"."+"addStudent";//全限定名称或mybatis命名空间(包名+类名+.+SQL唯一标识名)
-        int addResult = sqlSession.insert(sqlId,new Student(1320,"王五","1231312@qq.com",30));//查询全部集合数据
-        sqlSession.commit();
+        int addResult = sqlSession.insert(sqlId,new Student(1310,"王五","1231312@qq.com",30));//查询全部集合数据
         System.out.println(addResult);
         sqlSession.close();//关闭数据流
     }
